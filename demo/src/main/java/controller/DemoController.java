0a8ad4d9ebdc3971d2f564924a5207723813fbdb
Jinnie Tuan <jinnietuan@Jinnies-MacBook-Pro.local>
2021-02-20 21:50:14 +0700

    create

---
+++ demo/src/main/java/controller/DemoController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/h")
+    public String demo(){
+        return "hello";
+    }
+}
